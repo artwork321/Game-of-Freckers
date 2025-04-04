e851cf145375c335946a25f6aba3a1990ceb8289
Kylie <thuylinh1@student.unimelb.edu.au>
2025-04-04 14:15:55 +0000

    store target coords for faster heuristic computation; remove node's children tracking; change BFS mode to avoid unecessary computations

--- search/ai_utils.py
+++ search/ai_utils.py
@@ -4,14 +4,14 @@ import numpy as np
 
 BOARD_N = 8
 GOAL_ROW = 7
+BEST_FS = True # Set to False for Breadth-First Search (baseline)
 
-# Node: contain state, parent, action, depth, children, h(n), f(n), min (f(n))
+# Node contains state, parent, action, depth, h(n), f(n)
 class Node:
-    def __init__(self, state, parent, action: MoveAction, children, is_jump=False):
+    def __init__(self, state, parent, action: MoveAction, is_jump=False):
         self.state = state
         self.parent = parent
         self.action = action
-        self.children = children
         self.is_jump = is_jump
         
         if not parent:
@@ -24,26 +24,22 @@ class Node:
         else: 
             self.cost_so_far = parent.cost_so_far + 1
 
-        self.est_cost_to_goal = self.get_est_cost_to_goal()
+        self.est_cost_to_goal = self._get_est_cost_to_goal()
         # Check if there are no valid targets
         if self.est_cost_to_goal is None:
             self.est_total_cost = None
         else:  
             self.est_total_cost = self.cost_so_far + self.est_cost_to_goal
 
-    # TODO no need to keep track of children 
-    def add_children(self, child_node):
-        self.children.append(child_node)
-
-    def get_est_cost_to_goal(self):
-        if self.goal_test():
+    def _get_est_cost_to_goal(self): 
+        # Set heuristic to 0 for Breadth-First Search
+        if not BEST_FS or self.goal_test():
             return 0
 
         est_cost_to_goal = None 
-        for (coord, cell_state) in self.state.board.items():
-            if ((coord.r == GOAL_ROW and cell_state == CellState.LILY_PAD)
-                # Check if a BLUE is reachable using valid moves
-                or (coord.r >= self.state.red_frog_coord.r and cell_state == CellState.BLUE)): 
+        for coord in State.target_coords: # State.last_row_lily_pads_coords + State.blue_frogs_coords:
+            # Check if the coord is reachable (on a higher row than that of the red frog)
+            if coord.r >= self.state.red_frog_coord.r:
                 est_distance = self._get_est_distance(coord)
                 if est_cost_to_goal is None:
                     est_cost_to_goal = est_distance
@@ -53,18 +49,57 @@ class Node:
         return est_cost_to_goal
 
     def _get_est_distance(self, target: Coord):
-        dist_vector = Vector2(*target) - Vector2(*self.state.red_frog_coord)
-        n_diag_moves = min(abs(dist_vector.r), abs(dist_vector.c)) 
-        dist_vector -= Vector2(*map(np.sign, dist_vector)) * n_diag_moves
-        n_verti_moves = abs(dist_vector.r)
-        n_horiz_moves = abs(dist_vector.c)
+        # dist_vector = Vector2(*target) - Vector2(*self.state.red_frog_coord)
+        # n_diag_moves = min(abs(dist_vector.r), abs(dist_vector.c)) 
+        # dist_vector -= Vector2(*map(np.sign, dist_vector)) * n_diag_moves
+        # n_verti_moves = abs(dist_vector.r)
+        # n_horiz_moves = abs(dist_vector.c)
+        
+        verti_dist = abs(target.r - self.state.red_frog_coord.r)
+        horiz_dist = abs(target.c - self.state.red_frog_coord.c)
+        n_diag_moves = min(verti_dist, horiz_dist)
+        
+        # n_verti_moves = abs(verti_dist) - n_diag_moves
+        # n_horiz_moves = abs(horiz_dist) - n_diag_moves
 
-        return n_diag_moves + n_verti_moves + n_horiz_moves
-    
+        # return n_diag_moves + n_verti_moves + n_horiz_moves
+        return verti_dist + horiz_dist - n_diag_moves
+
     # Evaluate whether the given node satisfies the goal of the problem
     def goal_test(self):
         return self.state.red_frog_coord.r == GOAL_ROW
     
+    # Apply action, check if action is valid
+    def apply_action(self, direction: Direction):
+        current_state = self.state
+        new_state = State(self.state.board, self.state.red_frog_coord)
+        is_jump = False
+
+        try: 
+            new_coord = current_state.red_frog_coord + direction
+        except ValueError as e:
+            return None
+        
+        # Update state for valid move
+        if (new_coord in current_state.board and current_state.board[new_coord] == CellState.BLUE): # Can jump
+            try:
+                new_coord = new_coord + direction
+                is_jump = True
+            except ValueError:
+                return None  # Invalid jump move
+
+        if (new_coord in current_state.board and current_state.board[new_coord] == CellState.LILY_PAD): # Valid lily pad
+            
+            new_state.red_frog_coord = new_coord
+            new_state.board[new_state.red_frog_coord] = CellState.RED # Move Red frog to new cell
+            new_state.board.pop(current_state.red_frog_coord)
+
+            new_node = Node(new_state, self, MoveAction(current_state.red_frog_coord, direction), is_jump)
+
+            return new_node
+        else:
+            return None
+
     #  Return the sequence of actions that generated the given the node
     def get_path(self, display=False):
         path = []
@@ -89,8 +124,12 @@ class Node:
         return render_board(self.state.board, ansi=True)     
 
 
-# State: contain red frog position, board = dict[Coord, CellState]
+# State contains  red frog position, board = dict[Coord, CellState]
 class State:
+    # Store the coordinates of blue frogs and last row lily pads
+    # to compute heuristics later on 
+    target_coords = []
+
     def __init__(self, board: dict[Coord, CellState], red_frog_coord: Coord):
         self.board = board.copy()
 
@@ -102,35 +141,12 @@ class State:
         else:
             self.red_frog_coord = Coord(red_frog_coord.r, red_frog_coord.c)
 
+    @classmethod
+    def get_target_pos(cls, board: dict[Coord, CellState]):
+        if BEST_FS:
+            for (coord, cell_state) in board.items():
+                if cell_state == CellState.BLUE: 
+                    cls.target_coords.append(coord)
+                elif coord.r == GOAL_ROW and cell_state == CellState.LILY_PAD:
+                    cls.target_coords.append(coord)
 
-# apply action, check if action is valid
-# TODO add this as a method of Node class 
-def apply_action(direction: Direction, node: Node):
-    current_state = node.state
-    new_state = State(node.state.board, node.state.red_frog_coord)
-    is_jump = False
-
-    try: 
-        new_coord = current_state.red_frog_coord + direction
-    except ValueError as e:
-        return None
-    
-    # Update state for valid move
-    if (new_coord in current_state.board and current_state.board[new_coord] == CellState.BLUE): # Can jump
-        try:
-            new_coord = new_coord + direction
-            is_jump = True
-        except ValueError:
-            return None  # Invalid jump move
-
-    if (new_coord in current_state.board and current_state.board[new_coord] == CellState.LILY_PAD): # Valid lily pad
-        
-        new_state.red_frog_coord = new_coord
-        new_state.board[new_state.red_frog_coord] = CellState.RED # Move Red frog to new cell
-        new_state.board.pop(current_state.red_frog_coord)
-
-        new_node = Node(new_state, node, MoveAction(current_state.red_frog_coord, direction), [], is_jump)
-
-        return new_node
-    else:
-        return None
--- search/program.py
+++ search/program.py
@@ -13,37 +13,30 @@ def search(
     board: dict[Coord, CellState]
 ) -> list[MoveAction] | None:
     """
-    This is the entry point for your submission. You should modify this
-    function to solve the search problem discussed in the Part A specification.
-    See `core.py` for information on the types being used here.
+    Solve the search problem discussed in the Part A specification.
 
     Parameters:
         `board`: a dictionary representing the initial board state, mapping
             coordinates to "player colours". The keys are `Coord` instances,
             and the values are `CellState` instances which can be one of
             `CellState.RED`, `CellState.BLUE`, or `CellState.LILY_PAD`.
-    
+
     Returns:
         A list of "move actions" as MoveAction instances, or `None` if no
         solution is possible.
     """
+    print(render_board(board, ansi=True))
+
     node_count = 0 
     time_start = time.perf_counter()
 
-    # The render_board() function is handy for debugging. It will print out a
-    # board state in a human-readable format. If your terminal supports ANSI
-    # codes, set the `ansi` flag to True to print a colour-coded version!
-    print(render_board(board, ansi=True))
-
-    # Do some impressive AI stuff here to find the solution...
-    init_node = Node(State(board, None), None, None, [])
+    State.get_target_pos(board)
+    init_node = Node(State(board, None), None, None)
     priority_queue = StablePriorityQueue()
-    queuing_fn_BestFS(priority_queue, init_node)
-    # queuing_fn_BreadthFS(priority_queue, init_node)
+    queuing_fn(priority_queue, init_node)
 
     while True:
         if priority_queue.empty():
-            # no more possible states
             time_elapsed = (time.perf_counter() - time_start)
             memMb=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
             print ("%5.5f secs %5.5f MByte" % (time_elapsed,memMb))
@@ -60,44 +53,12 @@ def search(
             return next_node.get_path(display=False)
 
         for dir in DIR_ACTIONS:
-            new_node = apply_action(dir, next_node)
+            new_node = next_node.apply_action(dir)
 
             if new_node is not None:
-                next_node.add_children(new_node)
-                queuing_fn_BestFS(priority_queue, new_node)
-                # queuing_fn_BreadthFS(priority_queue, new_node)
+                queuing_fn(priority_queue, new_node)
                 node_count += 1
-                
-                # print(new_node)
-                # multiple_jumps_node = []
 
-                # if (new_node.is_jump):
-                #     multiple_jumps_node = expand_node_jump(new_node)
-
-                # for node in multiple_jumps_node:
-                #     next_node.add_children(node)
-                #     queuing_fn_BreadthFS(priority_queue, node)
-
-def expand_node_jump(node, multiple_jumps = []):
-    for dir in DIR_ACTIONS:
-        new_node = apply_action(dir, node)
-
-        # Only allow multiple jumps
-        if (new_node is not None and new_node.is_jump):
-            # Merge two nodes
-            new_node.action = MoveAction(node.action.coord, node.action.directions + new_node.action.directions)
-            new_node.parent = node.parent
-            new_node.cost_so_far = node.cost_so_far
-
-            multiple_jumps.append(new_node)
-            expand_node_jump(new_node, multiple_jumps)
-        
-    return multiple_jumps
-
-def queuing_fn_BreadthFS(queue: StablePriorityQueue, node: Node) -> None:
-    node.est_total_cost = node.cost_so_far
-    queue.put(node)
-
-def queuing_fn_BestFS(queue: StablePriorityQueue, node: Node) -> None:
+def queuing_fn(queue: StablePriorityQueue, node: Node) -> None:
     if node.est_total_cost is not None:
         queue.put(node)
